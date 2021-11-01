23fb41aeffec3c9f84a2646e661cb7792d32c88e
otomokenji <kenji.otomo@rakus-partners.co.jp>
2021-11-01 17:12:34 +0900

    演習２が完成しました２

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -5,16 +5,34 @@ import javax.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * 足し算結果を表示します
+ * 
+ * @author ootomokenji
+ *
+ */
 @Controller
 @RequestMapping("/exam02")
 public class Exam02Controller {
 
+	/**
+	 * 最初のページに飛びます
+	 * 
+	 * @return
+	 */
 	@RequestMapping("")
 	public String index() {
 		
 		return "exam02";
 	}
 	
+	/**
+	 * 数字を２つ受け取り、２つの数字と足した数をセットして時間内であれば取り出せるようにします
+	 * @param num1　1つ目の数字をセット
+	 * @param num2　2つ目の数字をセット
+	 * @param session　セッションスコープ使います
+	 * @return　結果画面１に飛びます
+	 */
 	@RequestMapping("/result")
 	public String result(Integer num1,Integer num2,HttpSession session) {
 		
@@ -25,6 +43,9 @@ public class Exam02Controller {
 		return "exam02-result";
 	}
 	
+	/**
+	 * @return　結果画面２に飛びます
+	 */
 	@RequestMapping("/toPage2")
 	public String toPage2() {
 		return "exam02-result2";
