e4e7f4f2b3114b12bcb3abb57093bb59c7a4ef22
otomokenji <kenji.otomo@rakus-partners.co.jp>
2021-11-01 16:51:34 +0900

    Controller作りました

--- src/main/java/com/example/controller/Exam02Controller.java
+++ src/main/java/com/example/controller/Exam02Controller.java
@@ -1,5 +1,26 @@
 package com.example.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/exam02")
 public class Exam02Controller {
 
+	@RequestMapping("")
+	public String index() {
+		
+		return "exam02";
+	}
+	
+	@RequestMapping("/result")
+	public String result(Integer num1,Integer num2,Model model) {
+		
+		model.addAttribute("num1", num1);
+		model.addAttribute("num2", num2);
+		model.addAttribute("sum", num1+num2);
+		
+		return "exam02-result";
+	}
 }
